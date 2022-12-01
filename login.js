bc60610666a41ec4967de87fba287468ed276640
unknown <Prakashe121100@gmail.com>
2022-12-01 23:26:50 +0530

    logine_done

---
+++ login.js
@@ -0,0 +1,12 @@
+let prime=0;
+let num=13
+    for(i=1;i<=num;i++){
+        if(num%i==1)
+        prime++
+    }
+    if(prime==2){
+        console.log("Yes");
+    }
+    else{
+        console.log("No");
+  }
